8915d4b4d3fe04c74a1e849f3b22684c7e0217c7
Stephane de Labrusse <stephdl@de-labrusse.fr>
2022-10-26 15:44:19 +0200

    Build image script

--- build-images.sh
+++ build-images.sh
@@ -33,9 +33,9 @@ buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm" \
-    --label="org.nethserver.tcp-ports-demand=1" \
-    --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.tcp-ports-demand=0" \
+    --label="org.nethserver.rootfull=1" \
+    --label="org.nethserver.images=docker.io/crowdsecurity/crowdsec:v1.4.1-debian" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
