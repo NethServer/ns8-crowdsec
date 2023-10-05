298d0956f6b8cf38b1183416fad7a8ea9279c32b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-10-05 18:45:41 +0200

    upgrade to 1.5.4-debian

--- build-images.sh
+++ build-images.sh
@@ -34,7 +34,7 @@ buildah add "${container}" ui/dist /ui
 buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=" \
     --label="org.nethserver.rootfull=1" \
-    --label="org.nethserver.images=docker.io/crowdsecurity/crowdsec:v1.5.3-rc4-debian" \
+    --label="org.nethserver.images=docker.io/crowdsecurity/crowdsec:v1.5.4-debian" \
     --label="org.nethserver.tcp-ports-demand=2" \
     "${container}"
 # Commit the image
