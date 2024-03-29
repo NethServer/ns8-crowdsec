47b92e9a44626dd58f7fde9ff5db015d7bed1838
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-03-29 16:04:06 +0100

    Update crowdsec-firewall-bouncer version in build script

--- build-images.sh
+++ build-images.sh
@@ -12,19 +12,18 @@ set -e
 images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
-# versions can be found : https://github.com/crowdsecurity/cs-firewall-bouncer/releases
-bouncer_version="0.0.28"
 
 # Create a new empty container image fro crowdsec-firewall-bouncer
 reponame="crowdsec-firewall-bouncer"
 container=$(buildah from docker.io/debian:bookworm-20240311)
-buildah config --env BOUNCER_VERSION=${bouncer_version} --env DEBIAN_FRONTEND=noninteractive --env TZ=UTC "${container}"
+# The crowdsec-firewall-bouncer versions can be found : https://github.com/crowdsecurity/cs-firewall-bouncer/releases
+# change the version in the apt below
 buildah run "${container}" /bin/sh <<'EOF'
-apt update \
+export DEBIAN_FRONTEND=noninteractive && apt update \
     && apt upgrade -y \
     && apt install curl -y \
     && curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash \
-    && apt install crowdsec-firewall-bouncer-nftables=${BOUNCER_VERSION} -y \
+    && apt install crowdsec-firewall-bouncer-nftables=0.0.28 -y \
     && apt autoremove -y \
     && apt clean \
     && rm -rf /var/lib/apt/lists/*
