e28d1fee5c3e4009a76f9508138999c3c2e1bb21
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-02-07 12:57:55 +0100

    feat(build-images.sh): build bouncer from Alpine and install from github page

--- build-images.sh
+++ build-images.sh
@@ -12,27 +12,23 @@ set -e
 images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
-
+crowdsec_firewall_bouncer_version="v0.0.28"
 # Create a new empty container image fro crowdsec-firewall-bouncer
 reponame="crowdsec-firewall-bouncer"
-container=$(buildah from docker.io/debian:bookworm-20240311)
+container=$(buildah from docker.io/alpine:3.21.2)
 # The crowdsec-firewall-bouncer versions can be found : https://github.com/crowdsecurity/cs-firewall-bouncer/releases
 # change the version in the apt below
-buildah run "${container}" /bin/sh <<'EOF'
-export DEBIAN_FRONTEND=noninteractive
-apt update \
-    && apt upgrade -y \
-    && apt install curl -y \
-    && curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash \
-    && apt install crowdsec-firewall-bouncer-nftables=0.0.28 -y \
-    && apt autoremove -y \
-    && apt clean \
-    && rm -rf /var/lib/apt/lists/*
+buildah run --env crowdsec_firewall_bouncer_version=${crowdsec_firewall_bouncer_version} "${container}" /bin/sh <<'EOF'
+ wget -qO - \
+    "https://github.com/crowdsecurity/cs-firewall-bouncer/releases/download/${crowdsec_firewall_bouncer_version}/crowdsec-firewall-bouncer-linux-amd64.tgz" \
+    | tar -xz -C /tmp/ \
+    && mv /tmp/crowdsec-firewall-bouncer*/crowdsec-firewall-bouncer /usr/local/bin/crowdsec-firewall-bouncer \
+    && chmod +x /usr/local/bin/crowdsec-firewall-bouncer && rm -rf /tmp/*
 EOF
 
 buildah config \
     --workingdir="/" \
-    --cmd='["/usr/bin/crowdsec-firewall-bouncer", "-c", "/etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml"]' \
+    --cmd='["/usr/local/bin/crowdsec-firewall-bouncer", "-c", "/etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml"]' \
     --label="org.opencontainers.image.source=https://github.com/NethServer/ns8-crowdsec" \
     --label="org.opencontainers.image.authors=Stephane de Labrusse <stephdl@de-labrusse.fr>" \
     --label="org.opencontainers.image.title=Crowdsec-firewall-bouncer based on debian" \
