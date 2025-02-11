ffd2537f2285882d244ba47a132b27f5d0aa58c3
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-02-11 17:42:53 +0100

    Build bouncer from Alpine and install from GitHub page (#67)
    NethServer/dev#7307

--- build-images.sh
+++ build-images.sh
@@ -8,31 +8,35 @@
 # Terminate on error
 set -e
 
+# cleanup on exit
+cleanup_list=()
+trap 'rm -rf "${bouncer_tmp_dir}" crowdsec-firewall-bouncer-linux-amd64.tgz' EXIT
+
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 
-# Create a new empty container image fro crowdsec-firewall-bouncer
-reponame="crowdsec-firewall-bouncer"
-container=$(buildah from docker.io/debian:bookworm-20240311)
 # The crowdsec-firewall-bouncer versions can be found : https://github.com/crowdsecurity/cs-firewall-bouncer/releases
-# change the version in the apt below
-buildah run "${container}" /bin/sh <<'EOF'
-export DEBIAN_FRONTEND=noninteractive
-apt update \
-    && apt upgrade -y \
-    && apt install curl -y \
-    && curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash \
-    && apt install crowdsec-firewall-bouncer-nftables=0.0.28 -y \
-    && apt autoremove -y \
-    && apt clean \
-    && rm -rf /var/lib/apt/lists/*
-EOF
+curl --netrc --fail -L -o crowdsec-firewall-bouncer-linux-amd64.tgz \
+    https://github.com/crowdsecurity/cs-firewall-bouncer/releases/download/v0.0.31/crowdsec-firewall-bouncer-linux-amd64.tgz
+
+# After updates add the new value to CHECKSUM file: sha256sum crowdsec-firewall-bouncer-linux-amd64.tgz > CHECKSUM
+sha256sum -c CHECKSUM
+
+bouncer_tmp_dir=$(mktemp -d)
+tar -C "${bouncer_tmp_dir}" -x -v -z -f crowdsec-firewall-bouncer-linux-amd64.tgz
+
+# Create a new empty container image for crowdsec-firewall-bouncer
+reponame="crowdsec-firewall-bouncer"
+container=$(buildah from docker.io/alpine:3.21.2)
+
+# add to the container the crowdsec-firewall-bouncer
+buildah add --chmod 750 ${container} ${bouncer_tmp_dir}/crowdsec-firewall-bouncer-v*/crowdsec-firewall-bouncer /usr/local/bin/crowdsec-firewall-bouncer
 
 buildah config \
     --workingdir="/" \
-    --cmd='["/usr/bin/crowdsec-firewall-bouncer", "-c", "/etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml"]' \
+    --cmd='["/usr/local/bin/crowdsec-firewall-bouncer", "-c", "/etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml"]' \
     --label="org.opencontainers.image.source=https://github.com/NethServer/ns8-crowdsec" \
     --label="org.opencontainers.image.authors=Stephane de Labrusse <stephdl@de-labrusse.fr>" \
     --label="org.opencontainers.image.title=Crowdsec-firewall-bouncer based on debian" \
