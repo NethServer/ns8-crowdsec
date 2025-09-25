1cb36ac537387145db64ef74a148486fcbb4cd5c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-09-25 11:49:02 +0200

    fix(ui): update ConfirmReleaseIPAll component for loading state handling

--- ui/src/components/ConfirmReleaseIPAll.vue
+++ ui/src/components/ConfirmReleaseIPAll.vue
@@ -6,6 +6,8 @@
   <NsModal
     size="default"
     :visible="isShown"
+    :isLoading="isLoading"
+    :primary-button-disabled="isLoading"
     @modal-hidden="$emit('hide')"
     @primary-click="$emit('confirm')"
   >
@@ -14,22 +16,23 @@
       <div>{{ $t("unban.confirm_delete_all_bans_message") }}</div>
     </template>
     <template slot="secondary-button">{{ core.$t("common.cancel") }}</template>
-    <template slot="primary-button">{{ $t('unban.delete') }}</template>
+    <template slot="primary-button">{{ $t("unban.delete") }}</template>
   </NsModal>
 </template>
 
 <script>
 import { UtilService, IconService } from "@nethserver/ns8-ui-lib";
 export default {
-  name: "UnbanIPAll",
+  name: "ConfirmReleaseIPAll",
   mixins: [UtilService, IconService],
   props: {
     isShown: Boolean,
+    isLoading: Boolean,
     core: { type: Object },
   },
 };
-</script>
 
-<style scoped lang="scss">
+</script>
+<style scoped lang="scss">	
 @import "../styles/carbon-utils";
 </style>
