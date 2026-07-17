7cc3d94eb2623b9d9e6583035b48d7977e28599c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-07-17 12:29:08 +0200

    style(status): apply prettier formatting

--- ui/src/views/Status.vue
+++ ui/src/views/Status.vue
@@ -11,40 +11,58 @@
     </cv-row>
     <cv-row v-if="error.getStatus">
       <cv-column>
-        <NsInlineNotification kind="error" :title="$t('action.get-status')" :description="error.getStatus"
-          :showCloseButton="false" />
+        <NsInlineNotification
+          kind="error"
+          :title="$t('action.get-status')"
+          :description="error.getStatus"
+          :showCloseButton="false"
+        />
       </cv-column>
     </cv-row>
     <cv-row v-if="error.listBackupRepositories">
       <cv-column>
-        <NsInlineNotification kind="error" :title="$t('action.list-backup-repositories')"
-          :description="error.listBackupRepositories" :showCloseButton="false" />
+        <NsInlineNotification
+          kind="error"
+          :title="$t('action.list-backup-repositories')"
+          :description="error.listBackupRepositories"
+          :showCloseButton="false"
+        />
       </cv-column>
     </cv-row>
     <cv-row v-if="error.listBackups">
       <cv-column>
-        <NsInlineNotification kind="error" :title="$t('action.list-backups')" :description="error.listBackups"
-          :showCloseButton="false" />
+        <NsInlineNotification
+          kind="error"
+          :title="$t('action.list-backups')"
+          :description="error.listBackups"
+          :showCloseButton="false"
+        />
       </cv-column>
     </cv-row>
     <cv-row>
       <cv-column>
         <!-- card grid -->
-        <div class="
-            card-grid
-            grid-cols-1
-            md:grid-cols-2
-            xl:grid-cols-3
-            3xl:grid-cols-4
-          ">
+        <div
+          class="card-grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4"
+        >
           <!-- application -->
-          <NsInfoCard light :title="status.instance || '-'" :description="$t('status.application')"
-            :icon="Application32" :loading="loading.getStatus" class="min-height-card">
+          <NsInfoCard
+            light
+            :title="status.instance || '-'"
+            :description="$t('status.application')"
+            :icon="Application32"
+            :loading="loading.getStatus"
+            class="min-height-card"
+          >
             <template slot="content">
               <div class="card-rows">
                 <div class="card-row">
-                  <NsButton kind="ghost" :icon="Restart20" :disabled="loading.getStatus || !status.node"
-                    @click="isShownRestartModuleModal = true">
+                  <NsButton
+                    kind="ghost"
+                    :icon="Restart20"
+                    :disabled="loading.getStatus || !status.node"
+                    @click="isShownRestartModuleModal = true"
+                  >
                     {{ $t("status.restart_application") }}
                   </NsButton>
                 </div>
@@ -52,13 +70,23 @@
             </template>
           </NsInfoCard>
           <!-- installation node -->
-          <NsInfoCard light :title="installationNodeTitle" :titleTooltip="installationNodeTitleTooltip"
-            :description="$t('status.installation_node')" :icon="Chip32" :loading="loading.getStatus"
-            class="min-height-card">
+          <NsInfoCard
+            light
+            :title="installationNodeTitle"
+            :titleTooltip="installationNodeTitleTooltip"
+            :description="$t('status.installation_node')"
+            :icon="Chip32"
+            :loading="loading.getStatus"
+            class="min-height-card"
+          >
             <template slot="content">
               <div class="card-rows">
                 <div class="card-row">
-                  <NsButton kind="ghost" :icon="ArrowRight20" @click="goToNodeDetails()">
+                  <NsButton
+                    kind="ghost"
+                    :icon="ArrowRight20"
+                    @click="goToNodeDetails()"
+                  >
                     {{ $t("status.go_to_node_details") }}
                   </NsButton>
                 </div>
@@ -66,23 +94,46 @@
             </template>
           </NsInfoCard>
           <!-- backup -->
-          <NsBackupCard :title="core.$t('backup.title')" :noBackupMessage="core.$t('backup.no_backup_configured')"
-            :goToBackupLabel="core.$t('backup.go_to_backup')" :repositoryLabel="core.$t('backup.repository')"
-            :statusLabel="core.$t('common.status')" :statusSuccessLabel="core.$t('common.success')"
-            :statusNotRunLabel="core.$t('backup.backup_has_not_run_yet')" :statusErrorLabel="core.$t('error.error')"
-            :completedLabel="core.$t('backup.completed')" :durationLabel="core.$t('backup.duration')"
-            :totalSizeLabel="core.$t('backup.total_size')" :totalFileCountLabel="core.$t('backup.total_file_count')"
-            :backupDisabledLabel="core.$t('common.disabled')" :showMoreLabel="core.$t('common.show_more')"
-            :multipleUncertainStatusLabel="core.$t('backup.some_backups_failed_or_are_pending')
-              " :moduleId="instanceName" :moduleUiName="instanceLabel" :repositories="backupRepositories"
-            :backups="backups" :loading="loading.listBackupRepositories || loading.listBackups" :coreContext="core"
-            light />
+          <NsBackupCard
+            :title="core.$t('backup.title')"
+            :noBackupMessage="core.$t('backup.no_backup_configured')"
+            :goToBackupLabel="core.$t('backup.go_to_backup')"
+            :repositoryLabel="core.$t('backup.repository')"
+            :statusLabel="core.$t('common.status')"
+            :statusSuccessLabel="core.$t('common.success')"
+            :statusNotRunLabel="core.$t('backup.backup_has_not_run_yet')"
+            :statusErrorLabel="core.$t('error.error')"
+            :completedLabel="core.$t('backup.completed')"
+            :durationLabel="core.$t('backup.duration')"
+            :totalSizeLabel="core.$t('backup.total_size')"
+            :totalFileCountLabel="core.$t('backup.total_file_count')"
+            :backupDisabledLabel="core.$t('common.disabled')"
+            :showMoreLabel="core.$t('common.show_more')"
+            :multipleUncertainStatusLabel="
+              core.$t('backup.some_backups_failed_or_are_pending')
+            "
+            :moduleId="instanceName"
+            :moduleUiName="instanceLabel"
+            :repositories="backupRepositories"
+            :backups="backups"
+            :loading="loading.listBackupRepositories || loading.listBackups"
+            :coreContext="core"
+            light
+          />
           <!-- system logs -->
-          <NsSystemLogsCard :title="core.$t('system_logs.card_title')" :description="core.$t('system_logs.card_description', {
-            name: instanceLabel || instanceName,
-          })
-            " :buttonLabel="core.$t('system_logs.card_button_label')" :router="core.$router" context="module"
-            :moduleId="instanceName" light />
+          <NsSystemLogsCard
+            :title="core.$t('system_logs.card_title')"
+            :description="
+              core.$t('system_logs.card_description', {
+                name: instanceLabel || instanceName,
+              })
+            "
+            :buttonLabel="core.$t('system_logs.card_button_label')"
+            :router="core.$router"
+            context="module"
+            :moduleId="instanceName"
+            light
+          />
         </div>
       </cv-column>
     </cv-row>
@@ -108,23 +159,30 @@
         </cv-tile>
       </cv-column>
       <cv-column v-else>
-        <div class="
-            card-grid
-            grid-cols-1
-            md:grid-cols-2
-            xl:grid-cols-3
-            3xl:grid-cols-4
-          ">
-          <NsSystemdServiceCard v-for="service in failedServices" :key="service.name" light class="min-height-card"
-            :serviceName="service.name" :active="service.active" :failed="service.failed" :enabled="service.enabled"
-            :icon="Cube32" />
+        <div
+          class="card-grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4"
+        >
+          <NsSystemdServiceCard
+            v-for="service in failedServices"
+            :key="service.name"
+            light
+            class="min-height-card"
+            :serviceName="service.name"
+            :active="service.active"
+            :failed="service.failed"
+            :enabled="service.enabled"
+            :icon="Cube32"
+          />
         </div>
       </cv-column>
     </cv-row>
     <cv-row v-else>
       <cv-column :md="4" :max="4">
         <cv-tile light>
-          <cv-skeleton-text :paragraph="true" :line-count="4"></cv-skeleton-text>
+          <cv-skeleton-text
+            :paragraph="true"
+            :line-count="4"
+          ></cv-skeleton-text>
         </cv-tile>
       </cv-column>
     </cv-row>
@@ -138,20 +196,42 @@
       <cv-column>
         <cv-tile light>
           <div v-if="!loading.getStatus">
-            <NsEmptyState v-if="!status.images.length" :title="$t('status.no_images')">
+            <NsEmptyState
+              v-if="!status.images.length"
+              :title="$t('status.no_images')"
+            >
             </NsEmptyState>
-            <NsDataTable v-else :allRows="status.images" :columns="i18nImagesTableColumns"
-              :rawColumns="imagesTableColumns" :sortable="true" :pageSizes="[5, 10, 25, 50, 100]" :overflow-menu="false"
-              isSearchable :searchPlaceholder="$t('status.search_images')"
+            <NsDataTable
+              v-else
+              :allRows="status.images"
+              :columns="i18nImagesTableColumns"
+              :rawColumns="imagesTableColumns"
+              :sortable="true"
+              :pageSizes="[5, 10, 25, 50, 100]"
+              :overflow-menu="false"
+              isSearchable
+              :searchPlaceholder="$t('status.search_images')"
               :searchClearLabel="core.$t('common.clear_search')"
-              :noSearchResultsLabel="core.$t('common.no_search_results')" :noSearchResultsDescription="core.$t('common.no_search_results_description')
-                " :itemsPerPageLabel="core.$t('pagination.items_per_page')" :rangeOfTotalItemsLabel="core.$t('pagination.range_of_total_items')
-                " :ofTotalPagesLabel="core.$t('pagination.of_total_pages')"
-              :backwardText="core.$t('pagination.previous_page')" :forwardText="core.$t('pagination.next_page')"
-              :pageNumberLabel="core.$t('pagination.page_number')" @updatePage="imagesTablePage = $event">
+              :noSearchResultsLabel="core.$t('common.no_search_results')"
+              :noSearchResultsDescription="
+                core.$t('common.no_search_results_description')
+              "
+              :itemsPerPageLabel="core.$t('pagination.items_per_page')"
+              :rangeOfTotalItemsLabel="
+                core.$t('pagination.range_of_total_items')
+              "
+              :ofTotalPagesLabel="core.$t('pagination.of_total_pages')"
+              :backwardText="core.$t('pagination.previous_page')"
+              :forwardText="core.$t('pagination.next_page')"
+              :pageNumberLabel="core.$t('pagination.page_number')"
+              @updatePage="imagesTablePage = $event"
+            >
               <template slot="data">
-                <cv-data-table-row v-for="(row, rowIndex) in imagesTablePage" :key="`${rowIndex}`"
-                  :value="`${rowIndex}`">
+                <cv-data-table-row
+                  v-for="(row, rowIndex) in imagesTablePage"
+                  :key="`${rowIndex}`"
+                  :value="`${rowIndex}`"
+                >
                   <cv-data-table-cell>
                     <span>
                       {{ row.name }}
@@ -167,7 +247,11 @@
               </template>
             </NsDataTable>
           </div>
-          <cv-skeleton-text v-else :paragraph="true" :line-count="5"></cv-skeleton-text>
+          <cv-skeleton-text
+            v-else
+            :paragraph="true"
+            :line-count="5"
+          ></cv-skeleton-text>
         </cv-tile>
       </cv-column>
     </cv-row>
@@ -181,20 +265,42 @@
       <cv-column>
         <cv-tile light>
           <div v-if="!loading.getStatus">
-            <NsEmptyState v-if="!status.volumes.length" :title="$t('status.no_volumes')">
+            <NsEmptyState
+              v-if="!status.volumes.length"
+              :title="$t('status.no_volumes')"
+            >
             </NsEmptyState>
-            <NsDataTable v-else :allRows="status.volumes" :columns="i18nVolumesTableColumns"
-              :rawColumns="volumesTableColumns" :sortable="true" :pageSizes="[5, 10, 25, 50, 100]"
-              :overflow-menu="false" isSearchable :searchPlaceholder="$t('status.search_volumes')"
+            <NsDataTable
+              v-else
+              :allRows="status.volumes"
+              :columns="i18nVolumesTableColumns"
+              :rawColumns="volumesTableColumns"
+              :sortable="true"
+              :pageSizes="[5, 10, 25, 50, 100]"
+              :overflow-menu="false"
+              isSearchable
+              :searchPlaceholder="$t('status.search_volumes')"
               :searchClearLabel="core.$t('common.clear_search')"
-              :noSearchResultsLabel="core.$t('common.no_search_results')" :noSearchResultsDescription="core.$t('common.no_search_results_description')
-                " :itemsPerPageLabel="core.$t('pagination.items_per_page')" :rangeOfTotalItemsLabel="core.$t('pagination.range_of_total_items')
-                " :ofTotalPagesLabel="core.$t('pagination.of_total_pages')"
-              :backwardText="core.$t('pagination.previous_page')" :forwardText="core.$t('pagination.next_page')"
-              :pageNumberLabel="core.$t('pagination.page_number')" @updatePage="volumesTablePage = $event">
+              :noSearchResultsLabel="core.$t('common.no_search_results')"
+              :noSearchResultsDescription="
+                core.$t('common.no_search_results_description')
+              "
+              :itemsPerPageLabel="core.$t('pagination.items_per_page')"
+              :rangeOfTotalItemsLabel="
+                core.$t('pagination.range_of_total_items')
+              "
+              :ofTotalPagesLabel="core.$t('pagination.of_total_pages')"
+              :backwardText="core.$t('pagination.previous_page')"
+              :forwardText="core.$t('pagination.next_page')"
+              :pageNumberLabel="core.$t('pagination.page_number')"
+              @updatePage="volumesTablePage = $event"
+            >
               <template slot="data">
-                <cv-data-table-row v-for="(row, rowIndex) in volumesTablePage" :key="`${rowIndex}`"
-                  :value="`${rowIndex}`">
+                <cv-data-table-row
+                  v-for="(row, rowIndex) in volumesTablePage"
+                  :key="`${rowIndex}`"
+                  :value="`${rowIndex}`"
+                >
                   <cv-data-table-cell>
                     <span>
                       {{ row.name }}
@@ -210,12 +316,19 @@
               </template>
             </NsDataTable>
           </div>
-          <cv-skeleton-text v-else :paragraph="true" :line-count="10"></cv-skeleton-text>
+          <cv-skeleton-text
+            v-else
+            :paragraph="true"
+            :line-count="10"
+          ></cv-skeleton-text>
         </cv-tile>
       </cv-column>
     </cv-row>
-    <RestartModuleModal :visible="isShownRestartModuleModal" :node="status.node"
-      @hide="isShownRestartModuleModal = false" />
+    <RestartModuleModal
+      :visible="isShownRestartModuleModal"
+      :node="status.node"
+      @hide="isShownRestartModuleModal = false"
+    />
   </cv-grid>
 </template>
 
@@ -280,12 +393,7 @@ export default {
     };
   },
   computed: {
-    ...mapState([
-      "instanceName",
-      "instanceLabel",
-      "core",
-      "appName",
-    ]),
+    ...mapState(["instanceName", "instanceLabel", "core", "appName"]),
     installationNodeTitle() {
       if (this.status && this.status.node) {
         if (this.status.node_ui_name) {
